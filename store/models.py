1582757283e3aead32c00f5c2fc56281f19f294e
Yared Berhanu <yaredjlix@gmail.com>
2025-03-07 02:25:00 +0300

    addmin order model

--- store/models.py
+++ store/models.py
@@ -26,3 +26,14 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Cart"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user.username}"
